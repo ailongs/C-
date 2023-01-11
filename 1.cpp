64146ef45c7aaab284774e079d5643a901cab8fb
ailongs <112716217+ailongs@users.noreply.github.com>
2023-01-11 23:44:58 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,45 @@
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "hello world" << endl;
+    system("pause");
+    return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+main()
+{
+    int a=5;
+    cout << "a=" << a << endl;
+    system("pause");
+    return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+/*
+main()
+{
+    short num1=10;
+    int a=3e2;
+    float b=3e-2;
+    cout << "short占用的内存空间为：" << sizeof(num1) << endl;
+    cout << "int占用的内存空间为：" << sizeof(int) << endl;
+    cout << a << endl;
+    cout << b << endl;
+
+}
+*/
+/*
+main()
+{
+    char ch = 'a';
+    cout << "char占用的内存空间为:" << sizeof(char) << endl;
+    cout << int(ch) << endl;
+}
+*/
+
